948e3f517f582b3298d52ebab64f0bad94e4442f
Trey Bertram <113378171+treybertram06@users.noreply.github.com>
2025-03-30 23:07:29 -0700

    textures working

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "shader.h"
 #include "../include/glad/glad.h"
 #include "../include/GLFW/glfw3.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "../include/stb_image.h"
 
 using namespace std;
 
@@ -50,78 +52,95 @@ int main() {
 
     State state;
 
-    /*
-    float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-       -0.5f, -0.5f, 0.0f,  // bottom left
-       -0.5f,  0.5f, 0.0f   // top left
-   };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    };
-    */
 
     float vertices[] = {
-        // positions         // colors
-        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-       -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
-   };
-
-    float tex_coords[] = {
-        0.0f, 0.0f,  // lower-left corner
-        1.0f, 0.0f,  // lower-right corner
-        0.5f, 1.0f   // top-center corner
+        // positions          // colors           // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+       -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+       -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
     };
 
-
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
 
     if (!initialize(state))
         return -1;
 
     Shader shader("../src/shader.vert", "../src/shader.frag");
 
-    unsigned int VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
     //copy the vertices array in a buffer for OpenGL to use
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    /*
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    */
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 
     //set the vertex attributes pointers
     //position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     //color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    //texture attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
 
+    //textures
 
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-
-    float border_color[] = { 1.0f, 1.0f, 0.0f, 1.0f };
-    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, border_color);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+    glGenTextures(1, &texture2);
+    //set texture wrapping / filtering options on currently bound texture
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+    int width, height, nr_channels;
+    unsigned char *data = stbi_load("../assets/wall.jpg", &width, &height, &nr_channels, 0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        clog << "Failed to load texture.\n";
+    }
+    stbi_image_free(data);
+
+    unsigned char *data2 = stbi_load("../assets/cardboard.jpg", &width, &height, &nr_channels, 0);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    if (data2) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data2);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        clog << "Failed to load texture.\n";
+    }
+    stbi_image_free(data2);
+
+
+    shader.use();
+    shader.set_int("texture1", 0);
+    shader.set_int("texture2", 1);
 
     while(!glfwWindowShouldClose(state.window)) {
 
@@ -133,20 +152,22 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //activate the shader
-        shader.use();
 
-        //update the uniform color, our_color
-        float time_val = glfwGetTime();
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        /*float time_val = glfwGetTime();
         float green_val = (sin(time_val) / 2.0f) + 0.5f;
         int vertex_color_loc = glGetUniformLocation(shader.ID, "our_color");
-        glUniform4f(vertex_color_loc, 0.0f, green_val, 0.0f, 1.0f);
+        glUniform4f(vertex_color_loc, 0.0f, green_val, 0.0f, 1.0f);*/
 
         // render triangles
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3); // if not using EBO
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // if using EBO of indices
-        //glBindVertexArray(0); // not sure what this does
+        //glDrawArrays(GL_TRIANGLES, 0, 3); // if not using EBO
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // if using EBO of indices
 
         // swap front and back buffer and call events
         glfwSwapBuffers(state.window);
--- src/shader.frag
+++ src/shader.frag
@@ -1,7 +1,13 @@
 #version 330 core
 out vec4 frag_color;
+
 in vec3 our_color;
+in vec2 tex_coord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main() {
-    frag_color = vec4(our_color, 1.0);
+    frag_color = mix(texture(texture1, tex_coord), texture(texture2, tex_coord), 0.5);
+    //frag_color = texture(texture2, tex_coord);
 }
